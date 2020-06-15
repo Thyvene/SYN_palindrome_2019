364647dbab7eb6fecfe0a18f99c87ed8d248e3df
karim <root@DESKTOP-6RT4O7B.localdomain>
2020-06-15 10:40:41 +0200

    [SYN] Fix Test_first_function_3 [Day 1]

--- src/main.c
+++ src/main.c
@@ -12,7 +12,7 @@ int main(int ac, char **av)
     if (ac != 3)
     {
         my_printf("Need 2 args\n");
-        return (84);
+        return (0);
     }
     if (av[1][0] == 'f') {
         printf("factorial of %d is %d\n", atoi(av[2]),
